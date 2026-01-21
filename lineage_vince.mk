deb1b30d8b651c9d96996c0d4618a41a2683f690
mizukanaAlt <rina301087@gmail.com>
2026-01-20 20:55:39 -0500

    vince: add SELINUX_IGNORE_NEVERALLOWS

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -18,6 +18,9 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
 
+# neverallows
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
 PRODUCT_NAME := lineage_vince
